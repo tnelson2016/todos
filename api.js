4bae2de6358c411ccadde3a296dc8eb9baf0dc62
Your Name <you@example.com>
2018-04-16 23:44:34 -0400

    'GET-todos'

--- api.js
+++ api.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+console.log('process.env', process.env)
+
+const todos = [
+  { id: 1, text: 'Wake up', completed: false },
+  { id: 2, text: 'Drink Coffee', completed: false },
+  { id: 3, text: 'Learn express', completed: false }
+]
+
 app.get('/', (req, res) => res.send('<h1> Welcome to the ToDos API</h1>'))
+
+app.get('/todos', (req, res) => res.send(todos))
 
 app.listen(4000, () => console.log('TODOS API is up on port', port))
