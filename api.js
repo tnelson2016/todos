e7cf32aaccafb1b3a30e8a5b14683c7090e022a3
Your Name <you@example.com>
2018-04-18 22:27:59 -0400

    added search on text and completed properties in array of objects

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const search = require('./lib/to-do-search')
 const {
   propOr,
   compose,
@@ -19,37 +20,19 @@ const port = propOr(9999, 'PORT', process.env)
 console.log('process.env.PORT', process.env.PORT)
 
 const todos = [
-  { id: 1, text: 'Wake up', completed: true },
-  { id: 2, text: 'Drink coffee', completed: true },
-  { id: 3, text: 'Drive to class', completed: true },
-  { id: 4, text: 'Learn Express', completed: true }
+  { id: 1, text: 'snore', completed: true },
+  { id: 2, text: 'wake up', completed: false },
+  { id: 3, text: 'drink coffee', completed: true },
+  { id: 4, text: 'drive to class', completed: false },
+  { id: 5, text: 'learn express', completed: true }
 ]
 
 app.get('/', (req, res) => res.send('<h1>Welcome to the ToDos API</h1>'))
 app.get('/todos', (req, res) => {
-  // GET /todos?s=text:teach
-  // req.query.s   // => `text:teach`
-
-  const isQuery = pathOr(null, ['query', 's'], req)
-
-  if (isQuery) {
-    const searchProp = compose(head, split(':'), prop('s'))(req.query)
-    const searchValue = compose(last, split(':'), prop('s'))(req.query)
-
-    //searchProp  // => 'text'
-    //searchValue // => 'teach'
-    console.log('searchProp', searchProp)
-    console.log('searchValue', searchValue)
-
-    var searchedToDos = []
-
-    function search(todo) {
-      return compose(contains(searchValue), split(' '))(todo[searchProp])
-    }
-
-    searchedToDos = filter(search, todos)
-
-    res.send(searchedToDos)
+  if (pathOr(null, [`query`, `s`], req)) {
+    const searchProp = compose(head, split(`:`), prop(`s`))(req.query)
+    const searchValue = compose(last, split(`:`), prop(`s`))(req.query)
+    res.send(filter(search(searchProp, searchValue), todos))
   } else {
     res.send(todos)
   }
